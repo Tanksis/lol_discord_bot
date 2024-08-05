9b4d4da87a7cd0825205eed9f237ea317fce7dd6
Eyad ElKhodary <eyad.elkhodary@outlook.com>
2024-08-05 12:43:34 -0400

    added simple commands

---
+++ deploy-commands.js
@@ -0,0 +1,58 @@
+const { REST, Routes } = require("discord.js");
+require("dotenv").config();
+
+const clientId = process.env.CLIENT_ID;
+const guildId = process.env.GUILD_ID;
+const token = process.env.DISCORD_API_KEY;
+const fs = require("node:fs");
+const path = require("node:path");
+
+const commands = [];
+//grab all commands
+const foldersPath = path.join(__dirname, "commands");
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+  //grab all command files from command directory
+  const commandsPath = path.join(foldersPath, folder);
+  const commandFiles = fs
+    .readdirSync(commandsPath)
+    .filter((file) => file.endsWith(".js"));
+  //grabs the output of each commands data for deployement
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+
+    if ("data" in command && "execute" in command) {
+      commands.push(command.data.toJSON());
+    } else {
+      console.log(
+        `WARNING: the command at ${filePath} is missing a required "data" or "execute" property`,
+      );
+    }
+  }
+}
+
+//construct instance of REST module
+const rest = new REST().setToken(token);
+
+//deploy commands
+(async () => {
+  try {
+    console.log(
+      `Started refreshing ${commands.length} application (/) commands.`,
+    );
+
+    //refresh all commands in guild
+    const data = await rest.put(
+      Routes.applicationGuildCommands(clientId, guildId),
+      { body: commands },
+    );
+
+    console.log(
+      `Succesfully reloaded ${data.length} application (/) commands.`,
+    );
+  } catch (error) {
+    console.error(error);
+  }
+})();
---
+++ index.js
@@ -0,0 +1,67 @@
+//require necessary discord.js classes
+require("dotenv").config();
+const fs = require("node:fs");
+const path = require("node:path");
+const { Client, Events, GatewayIntentBits, Collection } = require("discord.js");
+const token = process.env.DISCORD_API_KEY;
+//create new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.commands = new Collection();
+
+const foldersPath = path.join(__dirname, "commands");
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+  const commandsPath = path.join(foldersPath, folder);
+  const commandFiles = fs
+    .readdirSync(commandsPath)
+    .filter((file) => file.endsWith(".js"));
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+
+    if ("data" in command && "execute" in command) {
+      client.commands.set(command.data.name, command);
+    } else {
+      console.log(
+        `WARNING: the command at ${filePath} is missing a required "data" or "execute" property`,
+      );
+    }
+  }
+}
+
+client.on(Events.InteractionCreate, async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+  const command = interaction.client.commands.get(interaction.commandName);
+
+  if (!command) {
+    console.error(`no command matching ${interaction.commandName} was found.`);
+    return;
+  }
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    if (interaction.replied || interaction.deferred) {
+      await interaction.followUp({
+        content: "There was en error executing this command!",
+        ephemeral: true,
+      });
+    } else {
+      await interaction.reply({
+        content: "There was en error executing this command!",
+        ephemeral: true,
+      });
+    }
+  }
+  console.log(interaction);
+});
+//when client is ready, run this code once
+client.once(Events.ClientReady, (readyClient) => {
+  console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+//login with disc token
+client.login(token);
